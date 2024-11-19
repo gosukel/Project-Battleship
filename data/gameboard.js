dbd70171d6d05f45efb8083449f7b1b41b7f88c9
RJ Routh <94662443+gosukel@users.noreply.github.com>
2024-11-18 22:58:53 -0500

    add gameboard constructor, board layout, clearboard function, place function

---
+++ data/gameboard.js
@@ -0,0 +1,56 @@
+const Ship = require("./battleship");
+
+class Gameboard {
+  constructor() {
+    this.carrier = new Ship("Carrier");
+    this.battleship = new Ship("Battleship");
+    this.destroyer = new Ship("Destroyer");
+    this.submarine = new Ship("Submarine");
+    this.patrolBoat = new Ship("Patrol Boat");
+    this.ships = [
+      this.carrier,
+      this.battleship,
+      this.destroyer,
+      this.submarine,
+      this.patrolBoat,
+    ];
+
+    this.board = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+  }
+
+  clearBoard() {
+    this.board = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+  }
+
+  place(ship, x, y) {
+    if (!this.board[x] || !this.board[y]) return this.board;
+    for (let i = 0; i < ship.length; i++) {
+      this.board[x][y + i] = ship.id;
+    }
+    return this.board;
+  }
+}
+
+module.exports = Gameboard;
